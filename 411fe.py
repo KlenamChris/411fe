ef9d29892eae3a56398f59ba0a0759c33278382f
Klenam Chris <72560111+KlenamChris@users.noreply.github.com>
2025-12-29 05:20:33 +0000

    Remove unnecessary comment

--- 411fe.py
+++ 411fe.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import sys
 
-# CONSTANTS & CONFIGURATION 
+# CONSTANTS
 OPCODES = {
     'load', 'loadI', 'store', 'add', 'sub', 
     'mult', 'lshift', 'rshift', 'output', 'nop'
